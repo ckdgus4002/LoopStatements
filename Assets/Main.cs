e8fa11521ebef3e129bd875a500cb6f7626d7433
LCH <ckdgus4002@gmail.com>
2021-06-25 04:05:51 +0900

    Commit.

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,51 +1,99 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 namespace LCH
 {
     public class Main : MonoBehaviour
     {
-        [SerializeField] private float[,] times = { };
+        [SerializeField] private uint runNumber;
 
 
-        private int _index = -1;
+        private uint _defaultRunNumber;
+        private readonly List<float>[] _times =
+        {
+            new List<float>(),
+            new List<float>(),
+            new List<float>(),
+            new List<float>(),
+            new List<float>(),
+            new List<float>(),
+        };
 
-        
-        
-        [ContextMenu("Run")]
+
+        private void Awake()
+        {
+            _defaultRunNumber = runNumber;
+        }
+
+        private void Start()
+        {
+            Run();
+            DebugLog(10 < _defaultRunNumber);
+        }
+
+
         private void Run()
         {
-            _index = (_index + 1) % 5;
-            
-            var array = new int[100_000];
-            var enumerable = new IEnumerable<int>[100_000];
-            
-            var awake = DateTime.Now;
-            
-            var i = 0;
-            var start = DateTime.Now;
-            while (i < array.Length) { i++; }
-            times[0, _index] = (DateTime.Now - start).Ticks;
-            
-            start = DateTime.Now;
-            for (var j = 0; j < array.Length; j++) { }
-            times[1, _index] = (DateTime.Now - start).Ticks;
-            
-            start = DateTime.Now;
-            foreach (var item in array) { }
-            times[2, _index] = (DateTime.Now - start).Ticks;
+            while (0 < runNumber)
+            {
+                GC.Collect(2);
 
-            start = DateTime.Now;
-            foreach (var item in enumerable) { }
-            times[3, _index] = (DateTime.Now - start).Ticks;
-            
-            start = DateTime.Now;
-            foreach (var item in new IEnumerable<int>[100_000]) { }
-            times[4, _index] = (DateTime.Now - start).Ticks;
+                var start = new DateTime();
+                
+                var array = new int[100_000];
+                start = DateTime.Now;
+                for (var l = 0; l < array.Length; l++) { }
+                _times[0].Add((DateTime.Now - start).Ticks);
+                
+                start = DateTime.Now;
+                foreach (var item in array) { }
+                _times[1].Add((DateTime.Now - start).Ticks);
+                
+                var enumerable = new IEnumerable<int>[100_000];
+                start = DateTime.Now;
+                foreach (var item in enumerable) { }
+                _times[2].Add((DateTime.Now - start).Ticks);
 
-            Debug.Log((DateTime.Now - awake).Ticks);
+                var j = 0;
+                start = DateTime.Now;
+                while (j < array.Length) { j++; }
+                _times[3].Add((DateTime.Now - start).Ticks);
+
+                // start = DateTime.Now;
+                // for (var l = 0; l < new int[100_000].Length; l++) { }
+                // _times[4].Add((DateTime.Now - start).Ticks);
+                
+                // start = DateTime.Now;
+                // foreach (var item in new IEnumerable<int>[100_000]) { }
+                // _times[5].Add((DateTime.Now - start).Ticks);
+
+                runNumber--;
+            }
         }
+
+        private void DebugLog(bool onlyDebung)
+        {
+            foreach (var t in _times)
+            {
+                if (t == null || t.Count == 0) continue;
+                
+                if (onlyDebung)
+                {
+                    Debug.Log(t.Average());
+                }
+                else
+                {
+                    var sb = new StringBuilder();
+                    foreach (var t1 in t) sb.Append($"{t1} ");
+                    
+                    Debug.Log($"{sb}, 평균: {t.Average()}");
+                }
+            }
+        }
+        
     }
 }
